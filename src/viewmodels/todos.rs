43e42460ec7173f4c81567aab6c4a377f48ba332
Lando <lando.schumpich@gmail.com>
2024-03-03 15:18:51 +0100

    Move todo templates to their own folder

--- src/viewmodels/todos.rs
+++ src/viewmodels/todos.rs
@@ -2,20 +2,20 @@ use crate::api::types::todo::TodoItem;
 use askama::Template;
 
 #[derive(Template)]
-#[template(path = "todo.html")]
+#[template(path = "todos/todo.html")]
 pub(crate) struct TodoModel {
     pub todo: TodoItem,
 }
 
 #[derive(Template)]
-#[template(path = "todos.html")]
+#[template(path = "todos/todos.html")]
 pub(crate) struct TodoListModel {
     pub filter: String,
     pub todos: Vec<TodoItem>,
 }
 
 #[derive(Template)]
-#[template(path = "todo_index.html")]
+#[template(path = "todos/index.html")]
 pub(crate) struct TodoIndex {
     pub filter: String,
 }
